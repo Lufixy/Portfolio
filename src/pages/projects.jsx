4c3d84a19a593966509a00795a381e0383f9e190
Kalashi <57494449+Kalashiexe@users.noreply.github.com>
2022-11-04 00:39:43 +0300

    Update projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "../../config";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Projects({ data }) {
   return (
     <motion.div
@@ -29,11 +29,33 @@ export default function Projects({ data }) {
                 <div className="p-5">
                   <div className="flex justify-between items-center">
                     <div className=" items-center">
-                    <img
-                          className="w-full h-24 rounded-md  mr-4 "
-                          src={project?.image}
-                          alt={project?.name}
-                        />
+                    {project?.image ? (
+                      <Image
+                        src={project?.image}
+                        alt={project?.name}
+                        width={400}
+                        height={200}
+                        className="rounded-md"
+                      />
+                    ) : (
+                      <div className="bg-[#e2e3e5] dark:bg-[#1a1a1c] rounded-md w-10 h-10 flex justify-center items-center">
+                        <svg  
+                          xmlns="http://www.w3.org/2000/svg"  
+                          className="h-6 w-6 text-[#1a1a1c] dark:text-[#e2e3e5]"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                          />
+                        </svg>
+                      </div>
+                    )}
+                    
                       <div className="text-sm mt-2">
                         <p className="font-bold text-black dark:text-white">
                           {project?.name}
