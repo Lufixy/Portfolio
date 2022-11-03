fa268b888df0048cb996754fd6d3dbcd18d66dc8
Kalashi <57494449+Kalashiexe@users.noreply.github.com>
2022-11-04 00:40:04 +0300

    Update project.jsx

--- src/pages/api/v1/project.jsx
+++ src/pages/api/v1/project.jsx
@@ -4,7 +4,7 @@ export default async(req, res) => {
             "name": "Kalashi.me",
             "description": "A personal website built with Next.js and Tailwind CSS",
             "html_url": "https://github.com/Kalashiexe/Kalashi.me",
-            "image": "https://i.imgur.com/x7J5oG0"
+            "image": "https://i.imgur.com/x7J5oG0.png"
         },
         
 
