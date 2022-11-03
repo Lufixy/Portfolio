f09cab77b749f6938576121df8e1aa94c6b215d2
Kalashi <57494449+Kalashiexe@users.noreply.github.com>
2022-11-04 00:43:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    domains: ["kalashi.me", "/", "cdn.discordapp.com","raw.githubusercontent.com","sperovita.gallerycdn.vsassets.io","softwareer.net","www.coreldraw.com","ph-files.imgix.net","cdns.iconmonstr.com","avatars.githubusercontent.com"]
+    domains: ["kalashi.me", "/", "cdn.discordapp.com","i.imgur.com","raw.githubusercontent.com","sperovita.gallerycdn.vsassets.io","softwareer.net","www.coreldraw.com","ph-files.imgix.net","cdns.iconmonstr.com","avatars.githubusercontent.com",]
   },
 
 
