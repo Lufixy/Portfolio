249a933e6dae250150f1577a629e3cbd30567305
Kalashi <57494449+Kalashiexe@users.noreply.github.com>
2023-01-17 06:33:00 -0800

    Update project.jsx

--- src/pages/api/v1/project.jsx
+++ src/pages/api/v1/project.jsx
@@ -7,6 +7,13 @@ export default async(req, res) => {
             "html_url": "https://github.com/Kalashiexe/Kalashi.me"
         
         },
+        {
+            "name": "7bin.co",
+            "link": "https://cdn.discordapp.com/attachments/1061295779853123684/1064914867909296188/image.png",
+            "description": "A pastebin clone built with Next.js and Tailwind CSS",
+            "html_url": "https://7bin.co"
+        },
+
         
 
     ]
