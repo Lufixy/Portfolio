35b8056233925e2a59547e37774414208c736048
Kalashi <57494449+Kalashiexe@users.noreply.github.com>
2023-03-20 05:02:16 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,16 @@ const nextConfig = {
   images: {
     domains: ["kalashi.me", "/", "cdn.discordapp.com","i.imgur.com","raw.githubusercontent.com","sperovita.gallerycdn.vsassets.io","softwareer.net","www.coreldraw.com","ph-files.imgix.net","cdns.iconmonstr.com","avatars.githubusercontent.com",]
   },
-
+  
+ async redirects() {
+    return [
+      {
+        source: 'https://freepanel.xyz/logins',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 
 } 
 
